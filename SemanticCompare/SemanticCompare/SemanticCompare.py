0119018a96e0af39fc779ec9e219193508800813
wpcheng <wpcheng@iflytek.com>
2017-09-15 14:44:04 +0800

    完成语义比较

--- SemanticCompare/SemanticCompare/SemanticCompare.py
+++ SemanticCompare/SemanticCompare/SemanticCompare.py
@@ -7,67 +7,67 @@ correctEntities = []
 errorEntities = []
 
 def readActualResult():
-	#read actual results
-	actualResultFile = open("./ActualResults.txt", "r")
-	actualResult = actualResultFile.readlines()
-	actualResultFile.close()
+    #read actual results
+    actualResultFile = open("./ActualResults.txt", "r")
+    actualResult = actualResultFile.readlines()
+    actualResultFile.close()
 
-	i = 0;
-	while(i < len(actualResult)):
-		if(actualResult[i] == "Label_SpeechStart:\n"):
-			entity = SemanticEntity()
+    i = 0;
+    while(i < len(actualResult)):
+        if(actualResult[i] == "Label_SpeechStart:\n"):
+            entity = SemanticEntity()
 
-			if(i+1<len(actualResult)):
-				line = actualResult[i+1]
-			else:
-				break
+            if(i+1<len(actualResult)):
+                line = actualResult[i+1]
+            else:
+                break
 
-			if(not line.startswith("Label")):
-				param = line.split("\t")
-				entity.actualText = param[0]
-				entity.actualJson = param[2]
-				entity.isLocalResult = param[1]
-				entities.append(entity)
-			elif(line == "Label_SpeechError:\n"):
-				entities.append(entity)
-		i=i+1
-	return
+            if(not line.startswith("Label")):
+                param = line.split("\t")
+                entity.actualText = param[0]
+                entity.actualJson = param[2]
+                entity.isLocalResult = param[1]
+                entities.append(entity)
+            elif(line == "Label_SpeechError:\n"):
+                entities.append(entity)
+        i=i+1
+    return
 
 def readExpectText():
-	file = open("./ExpectText.txt", "r")
-	results = file.readlines()
-	file.close()
-	if(len(entities) != len(results)):
-		print("ExpectText Error, not match!")
-		return
-	for i in range(0, len(entities)):
-		entities[i].expectText = results[i]
-	return
+    file = open("./ExpectText.txt", "r")
+    results = file.readlines()
+    file.close()
+    if(len(entities) != len(results)):
+        print("ExpectText Error, not match!")
+        return
+    for i in range(0, len(entities)):
+        entities[i].expectText = results[i]
+    return
 
 def readExpectJson():
-	file = open("./ExpectJson.txt", "r")
-	results = file.readlines()
-	file.close()
-	if(len(entities) != len(results)):
-		print("ExpectJson Error, not match!")
-		return
-	for i in range(0, len(entities)):
-		entities[i].expectJson = results[i]
-	return
+    file = open("./ExpectJson.txt", "r")
+    results = file.readlines()
+    file.close()
+    if(len(entities) != len(results)):
+        print("ExpectJson Error, not match!")
+        return
+    for i in range(0, len(entities)):
+        entities[i].expectJson = results[i]
+    return
 
 def main():
-	readActualResult()
-	readExpectText()
-	readExpectJson()
+    readActualResult()
+    readExpectText()
+    readExpectJson()
 
-	for entity in entities:
-		if(entity.compare()):
-			correctEntities.append(entity)
-		else:
-			errorEntities.append(entity)
+    for entity in entities:
+        if(entity.compare()):
+            correctEntities.append(entity)
+        else:
+            errorEntities.append(entity)
 
-	print("correct count = %s" % len(correctEntities))
-	print("error count = %s" % len(errorEntities))
+    print("correct count = %s" % len(correctEntities))
+    print("error count = %s" % len(errorEntities))
 
 if __name__ == "__main__":
-	main()
+    main()
--- SemanticCompare/SemanticCompare/SemanticEntity.py
+++ SemanticCompare/SemanticCompare/SemanticEntity.py
@@ -1,33 +1,44 @@
-#created by wpcheng 
-#Sept. 14th, 2017
+#created by wpcheng
+#Sept.  14th, 2017
 import json
 import re
 
 class SemanticEntity(object):
-	actualText = ""
-	actualJson = ""
-	isLocalResult = -1
-	expectText = ""
-	expectJson = ""
-	
-	def __init__(self, actualText="", actualJson="", isLocalResult=-1):
-		self.actualText = actualText
-		self.actualJson = actualJson
-		self.isLocalResult = isLocalResult
-		return
-		
-	def cutText(self):
-		self.actualText.strip()
-		self.expectText.strip()
-		self.actualText = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？?、~@#￥%……&*（）]+", "", self.actualText)
-		self.expectText = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？?、~@#￥%……&*（）]+", "", self.expectText)
+    actualText = ""
+    actualJson = ""
+    isLocalResult = -1
+    expectText = ""
+    expectJson = ""
 
-	def compare(self):
-		self.cutText()
-		if(self.expectText == self.actualText):
-			return True
-		else:
-			return False
-		
-		
+    def __init__(self, actualText="", actualJson="", isLocalResult=-1):
+        self.actualText = actualText
+        self.actualJson = actualJson
+        self.isLocalResult = isLocalResult
+        return
 
+    def compare(self):
+        if(self.compareText() and self.compareSemantic()):
+            return True
+        else:
+            return False
+
+    def compareText(self):
+        self.actualText.strip()
+        self.expectText.strip()
+        self.actualText = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？?、~@#￥%……&*（）]+", "", self.actualText)
+        self.expectText = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？?、~@#￥%……&*（）]+", "", self.expectText)
+        if(self.expectText == self.actualText):
+            return True
+        else:
+            return False
+
+    def compareSemantic(self):
+        expect = json.loads(self.expectJson)
+        rawActual = json.loads(self.actualJson)
+        actual = {}
+        for key in expect.keys():
+            if(key in rawActual):
+                actual[key] = rawActual[key]
+            else:
+                return False
+        return actual == expect
